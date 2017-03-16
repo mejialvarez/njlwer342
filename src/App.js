8f21afa384c2277c62c09d2903011177315494ef
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2017-03-16 17:41:19 -0500

    Adds functionality of new tasks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react';
 
-/// Modifica el componente para que se puedan agregar tareas
-
 class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      tasks: ['Sacar la ropa', 'Hacer la cama', 'Leer un rato'],
+      taskInput: ''
+    }
+  }
+
+  onSubmitTask(event) {
+    event.preventDefault();
+    const task = this.state.taskInput;
+    this.setState({
+      tasks: this.state.tasks.concat(task),
+      taskInput: ''
+    });
+  }
+
+  onChangeTaskInput(event) {
+    this.setState({
+      taskInput: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {
+              this.state.tasks.map((name, index) => {
+                return <li key={index}>{name}</li>;
+              })
+            }
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit={this.onSubmitTask.bind(this)}>
+             <input type="text" id="new-task" value={this.state.taskInput} onChange={this.onChangeTaskInput.bind(this)} placeholder="Ingresa una tarea y oprime Enter" />
            </form>
         </div>
       </div>
-    )
+    );
   }
 }
 
